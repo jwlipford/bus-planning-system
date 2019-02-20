d88b6f5ae3b6ab5e2f23c7b66d4de4c840766c9f
jwlipford <jlipford@usca.edu>
2019-02-20 01:17:16 -0500

    ArrayList stations in CityDefaultInitialization is now read from CityStationsText.txt

--- CityDefaultInitialization.java
+++ CityDefaultInitialization.java
@@ -26,65 +26,32 @@ route is ~23 miles assuming straight line from SE to NE
 
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.*;
 
 public class CityDefaultInitialization
 {
-    public static void main( String[] args ) throws Exception
+    public static ArrayList<BusStation> stationsFileToArrayList() throws Exception
     {
-        ArrayList<BusStation> stations = new ArrayList<BusStation>(40);
-        
-        // Columns follow pattern: 1 2 3 n..., (n = n + 1); Longitude matches
-        // Rows follow pattern: 1 9 17 n..., (n = n + 8); Latitude matches
-		
-        // Column 1
-		stations.add( new BusStation( 33.883594, -84.548232, "Akkala" ) );	// NW Station
-		stations.add( new BusStation( 33.850408, -84.548232, "Eldin" ) );
-		stations.add( new BusStation( 33.817223, -84.548232, "Central" ) );
-		stations.add( new BusStation( 33.784037, -84.548232, "Faron" ) );
-		stations.add( new BusStation( 33.750851, -84.548232, "Hebra" ) );
-		stations.add( new BusStation( 33.717665, -84.548232, "Tabantha" ) );
-		stations.add( new BusStation( 33.684480, -84.548232, "Ridgeland" ) );
-		stations.add( new BusStation( 33.651294, -84.548232, "Lanayru" ) );	// SW Station
-			
-		// Column 2
-		stations.add( new BusStation( 33.883594, -84.479608, "Starborn Valley" ) );	// NNW Station
-		stations.add( new BusStation( 33.850408, -84.479608, "Star Haven" ) );
-		stations.add( new BusStation( 33.817223, -84.479608, "Crystal Palace" ) );
-		stations.add( new BusStation( 33.784037, -84.479608, "Windy Mill" ) );
-		stations.add( new BusStation( 33.750851, -84.479608, "Shiver City" ) );
-		stations.add( new BusStation( 33.717665, -84.479608, "Gusty Gulch" ) );
-		stations.add( new BusStation( 33.684480, -84.479608, "Star Way" ) );
-		stations.add( new BusStation( 33.651294, -84.479608, "Forever Forest" ) );	// SSW Station
-		
-		// Column 3
-		stations.add( new BusStation( 33.883594, -84.410984, "Agrabah" ) );	// N Station
-		stations.add( new BusStation( 33.850408, -84.410984, "Atlantica" ) );
-		stations.add( new BusStation( 33.817223, -84.410984, "Deep Jungle" ) );
-		stations.add( new BusStation( 33.784037, -84.410984, "Destiny Islands" ) );
-		stations.add( new BusStation( 33.750851, -84.410984, "Neverland" ) );
-		stations.add( new BusStation( 33.717665, -84.410984, "Olympus" ) );
-		stations.add( new BusStation( 33.684480, -84.410984, "Pride Lands" ) );
-		stations.add( new BusStation( 33.651294, -84.410984, "Timeless River" ) );	// S Station
-		
-		// Column 4
-		stations.add( new BusStation( 33.883594, -84.34236, "Besaid" ) );	// NNE Station
-		stations.add( new BusStation( 33.850408, -84.34236, "Kilika" ) );
-		stations.add( new BusStation( 33.817223, -84.34236, "Lucca" ) );
-		stations.add( new BusStation( 33.784037, -84.34236, "Bikanel" ) );
-		stations.add( new BusStation( 33.750851, -84.34236, "Macalania" ) );
-		stations.add( new BusStation( 33.717665, -84.34236, "Djose" ) );
-		stations.add( new BusStation( 33.684480, -84.34236, "Bevelle" ) );
-		stations.add( new BusStation( 33.651294, -84.34236, "Zanarkand" ) );	// NNW Station
-		
-		// Column 5
-		stations.add( new BusStation( 33.883594, -84.273736, "Stone Hill" ) );	// NE Station
-		stations.add( new BusStation( 33.850408, -84.273736, "Idol Springs" ) );
-		stations.add( new BusStation( 33.817223, -84.273736, "Alpine Ridge" ) );
-		stations.add( new BusStation( 33.784037, -84.273736, "Terrace Village" ) );
-		stations.add( new BusStation( 33.750851, -84.273736, "Sunny Villa" ) );
-		stations.add( new BusStation( 33.717665, -84.273736, "Shady Oasis" ) );
-		stations.add( new BusStation( 33.684480, -84.273736, "Cloud Spires" ) );
-		stations.add( new BusStation( 33.651294, -84.273736, "Crystal Islands" ) );	// SE Station
+    	File file = new File( "CityStationsText.txt" );
+    	BufferedReader br = new BufferedReader( new FileReader( file ) );
+    	ArrayList<BusStation> stations = new ArrayList<BusStation>();
+    	String line = br.readLine();
+    	while( line != null )
+    	{
+    		String[] splitLine = line.split( ", " );
+    		stations.add( new BusStation(
+    			Double.parseDouble( splitLine[0] ),
+    			Double.parseDouble( splitLine[1] ),
+    			splitLine[2].replaceAll( "\"", "" ) ) );
+    		line = br.readLine();
+    	}
+    	br.close();
+    	return stations;
+    }
+	
+	public static void main( String[] args ) throws Exception
+    {
+    	ArrayList<BusStation> stations = stationsFileToArrayList();
 		
 		// Columns 0-4
 		for( int c = 0; c < 5; ++c )
