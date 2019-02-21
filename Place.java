4e0fa267fdec1b0130c56b3a192e208b886bf0dd
jwlipford <jlipford@usca.edu>
2019-02-20 22:23:15 -0500

    Update Place class
    Removed two old, unnecessary methods.
    Created a simpler, streamlined format in which heading is displayed in headingTo method.
    Modified comments related to citations; added a general citation comment at top of file.

--- Place.java
+++ Place.java
@@ -1,3 +1,7 @@
+// Two methods in this class use information found at
+// https://www.movable-type.co.uk/scripts/latlong.html.
+// These methods' comments contain more information.
+
 public abstract class Place
 {
     public static final int EARTH_RADIUS = 3959; // miles
@@ -22,24 +26,6 @@ public abstract class Place
         this.longitude = longitude;
     }
     
-    public double milesTo_Old( Place place )
-    // 1° latitude constantly = ~69 miles, and 1° longitude varies from 0
-    // miles at the poles to a maximum of ~69 miles at the equator.
-    // Source: https://gis.stackexchange.com/questions/142326/
-    //
-    // This method works and is quicker than milesTo below, but this method is
-    // less accurate. I am leaving it here in case it is needed, but prefer the
-    // other one.
-    {
-    	double latDiff  = place.latitude  - this.latitude;
-        double longDiff = place.longitude - this.longitude;
-        
-        double vDiffMiles = 69 * latDiff;
-        double hDiffMiles = 69 * longDiff * Math.cos( Math.toRadians( latDiff ) );
-        
-        return Math.sqrt( vDiffMiles*vDiffMiles + hDiffMiles*hDiffMiles );
-    }
-    
     public double milesTo( Place place )
     // This method uses formulas found in the Distance section of
 	// https://www.movable-type.co.uk/scripts/latlong.html.
@@ -62,31 +48,16 @@ public abstract class Place
     
     public double hoursTo( Place place, Bus bus )
     {
-    	return milesTo( place ) / bus.getCruisingConsumption();
+    	return this.milesTo( place ) / bus.getCruisingConsumption();
     	// miles/(miles/hour) = miles*(hours/mile) = hours
     }
     
-    public double directionTo_old( Place place )
-    // Direction in degrees (CCW from East, as in Trig) to another place
-    {
-        double latDiff  = place.latitude  - this.latitude;
-        double longDiff = place.longitude - this.longitude;
-        
-        if( longDiff == 0 ) // Going straight up or down
-            return (latDiff > 0) ? 90 : 270;
-        
-        double arctan = Math.toDegrees( Math.atan( latDiff/longDiff ) );
-        // -90 < arctan < 90
-        
-        if( longDiff > 0 ) // Going right
-            return (arctan >= 0) ? arctan : (360 + arctan);
-        else // longDiff < 0; Going left
-            return 180 + arctan;
-    }
-    
     public double bearingTo( Place place )
-    // Direction in degrees (CW from North, as in Geography) to another place
-    // Also from https://www.movable-type.co.uk/scripts/latlong.html
+    // This method, like milesTo, uses a formula found in the Bearing section
+	// of https://www.movable-type.co.uk/scripts/latlong.html, though I applied
+    // a modulus since the given formula sometimes produced negative numbers.
+    // This method returns the direction in degrees (0° to 360°; clockwise from
+    // North, as in Geography) to another place.
     {
     	double thisLatRad   = Math.toRadians(  this.getLatitude()  );
     	double placeLatRad  = Math.toRadians( place.getLatitude()  );
@@ -98,33 +69,24 @@ public abstract class Place
     	double h = Math.cos( thisLatRad ) * Math.sin( placeLatRad );
     	double i = Math.sin( thisLatRad ) * Math.cos( placeLatRad ) * Math.cos(f);
     	
-    	return Math.toDegrees( Math.atan2( g, h-i ) );
+    	return Math.toDegrees( Math.atan2( g, h-i ) ) % 360;
     }
     
-    // Returns a direction as text.
-    // TODO: Modify maybe
     public String headingTo( Place place )
+    // Returns a bearing as text
     {
-        // Old:
-    	//  double angle = directionTo_old( place );
-    	// Basically equivalent to:
-    	double angle = 90 - bearingTo( place );
-        
-        if(      angle ==   0 )     return "Due East";
-        else if( angle  <  45 )     return angle + "° North of East";
-        else if( angle ==  45 )     return "North-East";
-        else if( angle  <  90 )     return (90 - angle) + "° East of North";
-        else if( angle ==  90 )     return "Due North";
-        else if( angle  < 135 )     return (angle - 90) + "° West of North";
-        else if( angle == 135 )     return "North-West";
-        else if( angle  < 180 )     return (180 - angle) + "° North of West";
-        else if( angle == 180 )     return "Due West";
-        else if( angle  < 225 )     return (angle - 180) + "° South of West";
-        else if( angle == 225 )     return "South-West";
-        else if( angle  < 270 )     return (270 - angle) + "° West of South";
-        else if( angle == 270 )     return "Due South";
-        else if( angle  < 315 )     return (angle - 270) + "° East of South";
-        else if( angle == 315 )     return "South-East";
-        else                        return (360 - angle) + "° South of East";
+        // Convert to float to use fewer characters
+    	float bearing  = (float)this.bearingTo( place );
+    	
+    	if( 359 < bearing || bearing < 1 )
+    		return bearing + "° (North)";
+    	else if( 89 < bearing && bearing < 91 )
+    		return bearing + "° (East)";
+    	else if( 179 < bearing && bearing < 181 )
+    		return bearing + "° (South)";
+    	else if( 269 < bearing && bearing < 271 )
+    		return bearing + "° (West)";
+    	else
+    		return bearing + "°";
     }
 }
