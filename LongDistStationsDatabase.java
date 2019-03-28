60016bfba3d2b67bf621dc6d18e9c828e65b4481
jwlipford <jlipford@usca.edu>
2019-03-27 23:51:13 -0400

    Created LongDistStationsDatabase.toArray()
    Created this new method.
    Made inconsequential modifications at other places in the file.

--- LongDistStationsDatabase.java
+++ LongDistStationsDatabase.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.io.BufferedReader;
-import java.io.FileWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.FileReader;
 
 public class LongDistStationsDatabase
@@ -60,6 +60,11 @@ public class LongDistStationsDatabase
     		line = br.readLine();
     	}
     	br.close();
+	}
+	
+	public BusStation[] toArray()
+	{
+		return (BusStation[])lDStations.toArray();
 	}
 	
 	public void update() throws Exception
@@ -121,6 +126,8 @@ public class LongDistStationsDatabase
 			}
 		}
 	}
+	
+	
 	
 	// TESTING ONLY:
 	public static void main( String[] args ) throws Exception
